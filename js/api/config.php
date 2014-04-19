39073a2c7db2f5b7f7bd91c752a63879b197ac88
Yasmine <yasmine.m.f@live.co.uk>
2014-04-19 13:08:30 +0400

    twitter

---
+++ js/api/config.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'OM5lm0GpUXCcezdWf6uoQ');
+    define('CONSUMER_SECRET', 'tWAmGPYjcTeOAdMQsgfmPwfGyJY9yY00gue3wiEyU');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '173731158-1F51dF4kVgjT78s4V8miSDWKbQHUmIlMqzCitn8C');
+    define('ACCESS_SECRET', 'FPaWzv6gT7oy8JgxGhAb8BwLSdDZXQjZOL2MUa3379ZC2');
+    ?>

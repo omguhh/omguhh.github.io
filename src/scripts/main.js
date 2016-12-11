3669d3a7999c2c7fd1872e497352f9cdb16af278
omguhh <yasmine.m.f@live.co.uk>
2016-12-11 15:27:23 +0400

    sample page transition with barba

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,9 @@
 require.config({
     paths: {
         "wowjs": "https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min",
-        "snap":"http://tympanus.net/Tutorials/ShapeHoverEffectSVG/js/snap.svg-min"
+        "snap":"http://tympanus.net/Tutorials/ShapeHoverEffectSVG/js/snap.svg-min",
+        "Barba":"https://cdnjs.cloudflare.com/ajax/libs/barba.js/0.0.10/barba.min",
+        "TweenMax":"https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.1/TweenMax.min"
     }
 });
 
@@ -42,4 +44,81 @@ require(['wowjs', 'snap'], function() {
         }
     );
     wow.init();
+});
+
+require(['Barba','TweenMax'],function(Barba, TweenMax) {
+        Barba.Pjax.start();
+        var lastElementClicked;
+        var NextLink = document.querySelector('a.next');
+
+        Barba.Pjax.init();
+        Barba.Prefetch.init();
+
+        Barba.Dispatcher.on('linkClicked', function(el) {
+            lastElementClicked = el;
+        });
+
+        var MovePage = Barba.BaseTransition.extend({
+            start: function() {
+                this.originalThumb = lastElementClicked;
+
+                Promise
+                    .all([this.newContainerLoading, this.scrollTop()])
+                    .then(this.movePages.bind(this));
+            },
+
+            scrollTop: function() {
+                var deferred = Barba.Utils.deferred();
+                var obj = { y: window.pageYOffset };
+
+                TweenLite.to(obj, 0.4, {
+                    y: 0,
+                    onUpdate: function() {
+                        if (obj.y === 0) {
+                            deferred.resolve();
+                        }
+
+                        window.scroll(0, obj.y);
+                    },
+                    onComplete: function() {
+                        deferred.resolve();
+                    }
+                });
+
+                return deferred.promise;
+            },
+
+            movePages: function() {
+                var _this = this;
+                var goingForward = true;
+                this.updateLinks();
+
+                TweenLite.set(this.newContainer, {
+                    visibility: 'visible',
+                    xPercent: goingForward ? 100 : -100,
+                    position: 'fixed',
+                    left: 0,
+                    top: 0,
+                    right: 0
+                });
+
+                TweenLite.to(this.oldContainer, 0.6, { xPercent: goingForward ? -100 : 100 });
+                TweenLite.to(this.newContainer, 0.6, { xPercent: 0, onComplete: function() {
+                    TweenLite.set(_this.newContainer, { clearProps: 'all' });
+                    _this.done();
+                }});
+            },
+
+            updateLinks: function() {
+                NextLink.href = this.newContainer.dataset.next;
+            },
+
+            getNewPageFile: function() {
+                return Barba.HistoryManager.currentStatus().url.split('/').pop();
+            }
+        });
+
+        Barba.Pjax.getTransition = function() {
+            return MovePage;
+        };
 });
